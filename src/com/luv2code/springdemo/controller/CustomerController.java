00dc005edfe750fa53effb379a44e3d9f3e0047a
kgit1 <kgit1@rambler.ru>
2016-11-17 01:49:41 +0200

    @GetMapping @PostMapping

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -21,7 +22,23 @@ public class CustomerController {
 	CustomerDAO customerDAO;
 
 	//add requestMapping to give link for our method
-	@RequestMapping("/list")
+	//@RequestMapping("/list") - work for all HTTP request methods
+	
+	//usual choise
+	//GET method - good for debaging, bookmarks,email url
+	//limitation on data lenght near 1000 signs
+	//@RequestMapping(path-"/list". method=RequestMethod.GET)
+	//same as
+	//GetMapping("/list")
+	
+	//choise for paswords and files
+	//POST method - bad for bookmarks, no limits on data length,
+	//also can send binary data(like when u need to save file)
+	//@RequestMapping(path-"/list". method=RequestMethod.POST)
+	//same as
+	//PostMapping("/list")
+	
+	@GetMapping("/list")
 	//add model in method to hold and translate data
 	public String listCustomers(Model theModel) {
 
